99766fffb10e8cb75f8fba04d30268b6a53a8271
DoxrGitHub <132623420+DoxrGitHub@users.noreply.github.com>
2023-10-25 08:26:54 -0500

    requires instead of imports

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-import { createBareServer } from "@tomphttp/bare-server-node";
-import { createServer } from "node:http";
-import { hostname } from "node:os";
+const { createBareServer } = require("@tomphttp/bare-server-node");
+const http = require("http");
+const os = require("os");
 
 const bare = createBareServer("/bare/");
-const server = createServer();
+const server = http.createServer();
 
 server.on("request", (req, res) => {
   bare.routeRequest(req, res);
@@ -22,7 +22,7 @@ server.on("listening", () => {
 
   console.log("Listening on:");
   console.log(`\thttp://localhost:${address.port}`);
-  console.log(`\thttp://${hostname()}:${address.port}`);
+  console.log(`\thttp://${os.hostname()}:${address.port}`);
   console.log(
     `\thttp://${
       address.family === "IPv6" ? `[${address.address}]` : address.address
